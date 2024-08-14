359148577fd44683dd4ecbc18add844e33045f3d
OlgerQuispe <goldsag43@gmail.com>
2024-08-14 17:18:10 -0500

    "Primer commit"

---
+++ 10_mixins.dart
@@ -0,0 +1,58 @@
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+abstract class Ave extends Animal {}
+abstract class Pez extends Animal {}
+
+mixin class Volador {
+  void volar() => print('estoy volando!');
+}
+//hola
+
+mixin class Caminante {
+  void caminar() => print('estoy caminando!');
+}
+
+mixin class Nadador {
+  void nadar() => print('estoy nadando!');
+}
+
+
+class Delfin extends Mamifero with Nadador {}
+class Murcielago extends Mamifero with Volador, Caminante {}
+class Gato extends Mamifero with Caminante {}
+
+class Paloma extends Ave with Caminante, Volador {}
+class Pato extends Ave with Caminante, Volador, Nadador {}
+
+class Tiburon extends Pez with Nadador {}
+class PezVolador extends Pez with Nadador, Volador {}
+
+
+void main() {
+  
+  final flipper = Delfin();
+  flipper.nadar();
+  
+  final batman = Murcielago();
+  batman.caminar();
+  batman.volar();
+  
+  
+  final namor = Pato();
+  namor.caminar();
+  namor.volar();
+  namor.nadar();
+   
+}
+
+
+
+
+
+
+
+
+
+
+
